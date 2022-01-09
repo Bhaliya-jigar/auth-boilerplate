dcbf8916ea5a5b547668a88a5168ea46013cf057
Bhaliya-jigar <jigar.bhaliya@gmail.com>
2022-01-09 20:21:35 +0530

    controller annotation

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -8,11 +8,22 @@ use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
 /**
- * @OA\Info(
- *    title="Authentication Boilerplate",
- *    version="1.0.0",
- * )
- */
+     * @OA\Info(
+     *      version="1.0.0",
+     *      title="Integration Swagger in Laravel with Auth Documentation",
+     *      description="Implementation of Swagger with in Laravel",
+     *      @OA\Contact(
+     *          email="admin@admin.com"
+     *      ),
+     *      @OA\License(
+     *          name="Apache 2.0",
+     *          url="http://www.apache.org/licenses/LICENSE-2.0.html"
+     *      )
+     * )
+     *
+     *
+     *
+     */
 
 class Controller extends BaseController
 {
